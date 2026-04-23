cbe874a199f3a988e82316b06fcfcfefe52ac8d2
Emelia Smith <ThisIsMissEm@users.noreply.github.com>
2026-04-23 21:10:17 +0200

    Move LazyResource to GermConvenience

---
+++ .changeset/thick-snails-know.md
@@ -0,0 +1,5 @@
+---
+"@germ-network/oauth4swift": patch
+---
+
+Move LazyResource to GermConvenience
--- Package.swift
+++ Package.swift
@@ -16,7 +16,7 @@ let package = Package(
 	dependencies: [
 		.package(
 			url: "https://github.com/germ-network/GermConvenience.git",
-			from: "0.1.2"
+			from: "0.1.3"
 		),
 		.package(url: "https://github.com/apple/swift-http-types.git", from: "1.5.1"),
 		.package(
